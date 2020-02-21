7dad0d52fe7632ba7692b243e284778ed3a304c6
Andrei Pranko <djmentarey@gmail.com>
2020-02-21 13:53:44 +0300

    added support switching app in different theme depends from ThemeSupport

--- app/src/main/java/com/mentarey/easyweather/MainActivity.kt
+++ app/src/main/java/com/mentarey/easyweather/MainActivity.kt
@@ -4,17 +4,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.compose.state
-import androidx.ui.core.Text
 import androidx.ui.core.setContent
-import androidx.ui.foundation.VerticalScroller
-import androidx.ui.layout.*
-import androidx.ui.material.MaterialTheme
 import androidx.ui.tooling.preview.Preview
-import androidx.ui.unit.dp
-import com.mentarey.easyweather.data.weather.model.current.CurrentWeather
-import com.mentarey.easyweather.data.weather.model.current.WeatherNow
 import com.mentarey.easyweather.ui.state.EasyWeatherScreenState
-import com.mentarey.easyweather.ui.widget.*
+import com.mentarey.easyweather.ui.widget.EasyWeatherAppBar
+import com.mentarey.easyweather.ui.widget.EasyWeatherContent
+import com.mentarey.easyweather.ui.widget.Scaffold
 import com.mentarey.easyweather.utils.observe
 import org.koin.androidx.viewmodel.ext.android.viewModel
 

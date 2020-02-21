ac55ebecda3e92f1cec1aa4643cd6e566033cf3f
Andrei Pranko <djmentarey@gmail.com>
2020-02-21 12:39:50 +0300

    added support switching app in different theme depends from ThemeSupport

--- app/src/main/java/com/mentarey/easyweather/ui/widget/ScaffoldWidget.kt
+++ app/src/main/java/com/mentarey/easyweather/ui/widget/ScaffoldWidget.kt
@@ -1,30 +1,56 @@
 package com.mentarey.easyweather.ui.widget
 
 import androidx.compose.Composable
-import androidx.compose.state
 import androidx.ui.foundation.isSystemInDarkTheme
 import androidx.ui.layout.Column
 import androidx.ui.layout.Container
 import androidx.ui.layout.LayoutHeight
+import androidx.ui.layout.LayoutWidth
+import androidx.ui.material.ColorPalette
 import androidx.ui.material.MaterialTheme
 import androidx.ui.material.darkColorPalette
 import androidx.ui.material.lightColorPalette
 import androidx.ui.unit.dp
 
 @Composable
-fun Scaffold(appBar: @Composable() () -> Unit, content: @Composable() () -> Unit) {
-    val currentThemePalette = when (isSystemInDarkTheme()) {
-        true -> darkColorPalette()
-        false -> lightColorPalette()
-    }
+fun Scaffold(
+    appThemeSupport: AppThemeSupport = AppThemeSupport.FollowSystem,
+    userLightColorPalette: ColorPalette = lightColorPalette(),
+    userDarkColorPalette: ColorPalette = darkColorPalette(),
+    appBar: @Composable() () -> Unit,
+    content: @Composable() () -> Unit
+) {
+    val currentThemePalette = appThemeSupport.getCurrentColorPalette(
+        lightColorPalette = userLightColorPalette,
+        darkColorPalette = userDarkColorPalette
+    )
+
     MaterialTheme(currentThemePalette) {
         Column {
-            Container(LayoutHeight(56.dp)) {
+            Container(LayoutHeight(56.dp) + LayoutWidth.Fill) {
                 appBar()
             }
             Container(LayoutFlexible(1f)) {
                 content()
             }
         }
+    }
+}
+
+sealed class AppThemeSupport {
+    object OnlyLight : AppThemeSupport()
+    object OnlyDark : AppThemeSupport()
+    object FollowSystem : AppThemeSupport()
+}
+
+private fun AppThemeSupport.getCurrentColorPalette(
+    lightColorPalette: ColorPalette = lightColorPalette(),
+    darkColorPalette: ColorPalette = darkColorPalette()
+): ColorPalette = when (this) {
+    is AppThemeSupport.OnlyLight -> lightColorPalette
+    is AppThemeSupport.OnlyDark -> darkColorPalette
+    is AppThemeSupport.FollowSystem -> when (isSystemInDarkTheme()) {
+        true -> darkColorPalette
+        false -> lightColorPalette
     }
 }
